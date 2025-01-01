bd436d6af20f97a46bda6f895750b0bb4464ef67
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2025-01-01 21:27:22 +0800

    use older kernel

--- modules/core/boot/kernel.nix
+++ modules/core/boot/kernel.nix
@@ -2,7 +2,7 @@
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_6;
     # This is for OBS Virtual Cam Support
     kernelModules = [ "v4l2loopback" ];
     extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
