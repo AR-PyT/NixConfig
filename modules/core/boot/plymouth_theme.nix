49f3d01e594c084a5ab407728ddd9ff8a2a019f3
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2025-01-01 19:57:17 +0800

    change directory structure

--- modules/core/boot/plymouth_theme.nix
+++ modules/core/boot/plymouth_theme.nix
@@ -5,8 +5,9 @@ in
 pkgs.stdenv.mkDerivation rec {
   pname = "theme_v1";
   version = "1.0.0";
-  src = themePath;
-  buildInputs = [ pkgs.plymouth ];
+  src = ./theme_v1.zip;
+  nativeBuildInputs = [ pkgs.unzip ];
+  buildInputs = [ pkgs.unzip pkgs.plymouth ];
 
   installPhase = ''
     mkdir -p $out/share/plymouth/themes/theme_v1
