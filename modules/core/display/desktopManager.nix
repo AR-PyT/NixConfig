a419f8dbb9769fed937f3575eedb27678c526d6a
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2025-01-01 13:25:39 +0800

    update core

--- modules/core/display/desktopManager.nix
+++ modules/core/display/desktopManager.nix
@@ -54,4 +54,15 @@
         hypridle
       ]
     };
+
+    # Portals
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+    ];
+    config.common.default = "hyprland";
+  };
 }
