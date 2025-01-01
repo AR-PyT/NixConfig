18fb7cfa6a6cbe4d8954ffaabcb488e4cf64843b
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2025-01-01 18:16:55 +0800

    update home-manager

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";  # For unstable packages as needed
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix.url = "github:danth/stylix";
--- hosts/home.nix
+++ hosts/home.nix
@@ -14,37 +14,32 @@ in
     userEmail = "${gitEmail}";
   };
 
-  home-manager.users.${user} = {
-    programs = {
-      home-manager.enable = true;
-    };
-    xdg = {
-      mime.enable = true;
-      mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "image/jpeg" = [ "image-roll.desktop" "feh.desktop" ];
-          "image/png" = [ "image-roll.desktop" "feh.desktop" ];
-          "text/plain" = "nvim.desktop";
-          "text/html" = "nvim.desktop";
-          "text/csv" = "nvim.desktop";
-          "application/pdf" = [ "wps-office-pdf.desktop" "firefox.desktop" ];
-          "application/zip" = "org.gnome.FileRoller.desktop";
-          "application/x-tar" = "org.gnome.FileRoller.desktop";
-          "application/x-bzip2" = "org.gnome.FileRoller.desktop";
-          "application/x-gzip" = "org.gnome.FileRoller.desktop";
-          "x-scheme-handler/http" = [ "firefox.desktop" ];
-          "x-scheme-handler/https" = [ "firefox.desktop" ];
-          "x-scheme-handler/about" = [ "firefox.desktop" ];
-          "x-scheme-handler/unknown" = [ "firefox.desktop" ];
-          "x-scheme-handler/mailto" = [ "gmail.desktop" ];
-          "audio/mp3" = "mpv.desktop";
-          "audio/x-matroska" = "mpv.desktop";
-          "video/webm" = "mpv.desktop";
-          "video/mp4" = "mpv.desktop";
-          "video/x-matroska" = "mpv.desktop";
-          "inode/directory" = "pcmanfm.desktop";
-        };
+  xdg = {
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/jpeg" = [ "image-roll.desktop" "feh.desktop" ];
+        "image/png" = [ "image-roll.desktop" "feh.desktop" ];
+        "text/plain" = "nvim.desktop";
+        "text/html" = "nvim.desktop";
+        "text/csv" = "nvim.desktop";
+        "application/pdf" = [ "wps-office-pdf.desktop" "firefox.desktop" ];
+        "application/zip" = "org.gnome.FileRoller.desktop";
+        "application/x-tar" = "org.gnome.FileRoller.desktop";
+        "application/x-bzip2" = "org.gnome.FileRoller.desktop";
+        "application/x-gzip" = "org.gnome.FileRoller.desktop";
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+        "x-scheme-handler/mailto" = [ "gmail.desktop" ];
+        "audio/mp3" = "mpv.desktop";
+        "audio/x-matroska" = "mpv.desktop";
+        "video/webm" = "mpv.desktop";
+        "video/mp4" = "mpv.desktop";
+        "video/x-matroska" = "mpv.desktop";
+        "inode/directory" = "pcmanfm.desktop";
       };
     };
   };
