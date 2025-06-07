b7b1366ba040d375776bb69eb53982b70e196568
Abdul Rehman <abrehman.bscs21seecs@seecs.edu.pk>
2025-06-07 15:47:07 +0800

    Update bootloader

--- modules/boot/bootloader.nix
+++ modules/boot/bootloader.nix
@@ -1,21 +1,58 @@
 { lib, pkgs, unstable,  ... }:
 let
   plymouth_theme = pkgs.stdenv.mkDerivation {
-        pname = "PlymouthTheme";
-        version = "1.1";
-        src = pkgs.fetchFromGitHub {
-          owner = "AR-PyT";
-          repo = "NixConfig";
-          rev = "plymouth-custom-theme";
-          hash = "sha256-8BAYxpclo/pa4L+/VpxDupmhWvdwXnbh8NUT2nSnea8=";
-        };
-        buildInputs = [ pkgs.plymouth ];
-        installPhase = ''
-          mkdir -p $out/share/plymouth/themes/hellonavi
-          cp -r hellonavi/* $out/share/plymouth/themes/hellonavi
-          cat hellonavi/hellonavi.plymouth | sed  "s@\/usr\/@$out\/@" > $out/share/plymouth/themes/hellonavi/hellonavi.plymouth
-        '';
+      pname = "PlymouthTheme";
+      version = "1.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "AR-PyT";
+        repo = "NixConfig";
+        rev = "plymouth-custom-theme";
+        hash = "sha256-8BAYxpclo/pa4L+/VpxDupmhWvdwXnbh8NUT2nSnea8=";
       };
+      buildInputs = [ pkgs.plymouth ];
+      installPhase = ''
+        mkdir -p $out/share/plymouth/themes/hellonavi
+        cp -r hellonavi/* $out/share/plymouth/themes/hellonavi
+        cat hellonavi/hellonavi.plymouth | sed  "s@\/usr\/@$out\/@" > $out/share/plymouth/themes/hellonavi/hellonavi.plymouth
+      '';
+    };
+
+    dynamic_grub_theme = pkgs.stdenv.mkDerivation {
+      pname = "GrubTheme";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "AR-PyT";
+        repo = "NixConfig";
+        rev = "grub";
+        hash = "sha256-GY5VLEynpl1R/kZyX6Db5zN8Rd7rZ/OirkSB3scfS0w=";
+      };
+
+      installPhase = ''
+        mkdir -p $out
+        cp -r ./* $out
+        
+        max_dir=-1
+
+        # Will assume that at least one directory exists
+        for dir in {0..59}; do
+          if [ -d "$out/$dir" ]; then
+            max_dir=$dir
+          fi
+        done
+
+        # Create links for directories that do not exist
+        for i in $(seq $(( max_dir+1 )) 59); do
+          # Calculate target directory in the cycle (20, 19, ..., 0, 20, ...)
+          target_idx=$(( (max_dir - (i % (max_dir + 1))) % (max_dir + 1) ))
+          if [ $target_idx -lt 0 ]; then
+            target_idx=$(( target_idx + max_dir + 1 ))
+          fi
+
+          # Create symbolic link
+          cp -r $out/$target_idx $out/$i
+        done
+      '';
+    };
 in
 {
   # Bootloader
@@ -27,17 +64,14 @@ in
       useOSProber = true;
       configurationLimit = 5;  # Limit NIXOS configs
 
-      theme = lib.mkForce (pkgs.stdenv.mkDerivation {
-        pname = "GrubTheme";
-        version = "1.2";
-        src = pkgs.fetchFromGitHub {
-          owner = "13atm01";
-          repo = "GRUB-Theme";
-          rev = "master";
-          hash = "sha256-4aW8IEkIFC3IZ/GdC2xLrFyDvbnBEvFyFHB4gsZxmWE=";
-        };
-        installPhase = "cp -r Kurumi/Kurumi $out";
-      });
+      theme = lib.mkForce dynamic_grub_theme;
+      extraConfig = ''
+        insmod datehook
+        load_env
+        set grub_background=($drive1)//theme/$SECOND/theme.txt
+        set theme=$grub_background
+        save_env grub_background
+      '';
     };
     timeout = 3;
     efi.canTouchEfiVariables = true;
